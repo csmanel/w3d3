d80e1a2cd993b30da571cd4c6ef153bb782d975c
Levi Sherman <Levi Sherman>
2023-11-01 14:03:57 -0400

    deep_dup and fib

--- practice-for-ch-ruby-recursion-long-practice-main/solution.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/solution.rb
@@ -21,7 +21,7 @@ def exp_two(base, power)
   return 1 if power == 0
   return base if power == 1
 
-  if power.even? 
+  if power.even?
     result = exp_two(base, power / 2)
     return result * result
   else
@@ -30,5 +30,37 @@ def exp_two(base, power)
   end
 end
 
-p exp_two(2, 2)
-p exp_two(2, 3)
+# p exp_two(2, 2)
+# p exp_two(2, 3)
+
+
+class Array
+  def deep_dup
+    new_arr = []
+    self.each do |el|
+      if el.is_a?(Array)
+        new_arr << el.deep_dup
+      else
+        new_arr << el
+      end
+    end
+    new_arr
+  end
+end
+
+
+# array = [1, [2], [3, [4]]]
+# p array.deep_dup
+
+
+def fib(n)
+  return [1, 1].take(n) if n <= 2
+
+  result = fib(n-1)
+  return result << result[-2] + result[-1]
+end
+
+# p fib(40)
+
+
+
