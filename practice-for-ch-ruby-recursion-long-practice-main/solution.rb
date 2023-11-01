75e66e605e9145cc7f8419ed8368459e12357e34
csmanel <csmanel@gmail.com>
2023-11-01 15:07:49 -0400

    make a binary search

--- practice-for-ch-ruby-recursion-long-practice-main/solution.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/solution.rb
@@ -62,5 +62,26 @@ end
 
 # p fib(40)
 
+def bsearch(arr, target)
+  middle_idx = arr.length / 2
+  return middle_idx if target == arr[middle_idx] 
+  return nil if arr.length <= 1
+  left = bsearch(arr[0...middle_idx], target)
+  right = bsearch(arr[middle_idx + 1..-1], target) 
 
+  if !left && !right 
+    return nil
+  elsif target < arr[middle_idx]
+    left
+  else
+    right + middle_idx + 1
+  end
+end
 
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
